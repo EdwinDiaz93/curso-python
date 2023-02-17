53bfe1c7d9deda71e92608bf9da6c9f28f8820cc
EdwinDiaz93 <josephph1993@gmail.com>
2023-02-17 11:47:15 -0600

    inicio de notas

---
+++ 01_python_basico/01_variables.py
@@ -0,0 +1,13 @@
+
+# Almacena un espacio en memoria y su valor
+# pueden almacenar distintos tipos de datos
+x=10
+y='hola'
+print(x)
+print(y)
+
+# se declaran de la siguiente forma 
+# sin caracteres especiales
+# no puede empezar por numeros
+# no pueden ser palabras reservadas
+# no pueden contener espacios
